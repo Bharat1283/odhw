6f19400e53887be38a6dcea1b8b40f961f2589b2
Neet UG <95118638+Bharat1283@users.noreply.github.com>
2022-09-10 17:04:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+function bedroom(){
+   window.location= "bedroom.html";
+}
+
+function bathroom(){
+   window.location= "bathroom.html";
+}
+
+function kitchen(){
+   window.location= "kitchen.html";
+}
+
+function gaming(){
+   window.location= "gaming.html";
+}
+
+function tv(){
+   window.location= "tv.html";
+}
+
+
+
